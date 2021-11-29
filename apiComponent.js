93cf79b29047664115ffe2a51a5901050e32c76d
Avinash S <avinashthebigboss@gmail.com>
2021-11-29 22:58:44 +0530

    Added Dragable element and Refactored Api func

--- apiComponent.js
+++ apiComponent.js
@@ -1,16 +1,13 @@
-let searchResult;
-
 export const books = async (title, author) => {
   try {
     const response = await fetch(
       `https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&key=AIzaSyA1Ci5u6z7Im-pWKx9JIJiKP2_H99Ok7lg`
     );
     const data = await response.json();
-    const items = [...data.items];
-    searchResult = items[1].volumeInfo;
-    console.log(searchResult.imageLinks.thumbnail);
-    console.log(searchResult);
+    const [items] = data.length >= 1 ? [...data.items] : [data];
+    return items.items[0].volumeInfo;
   } catch (err) {
     console.log(err.message);
+    throw err;
   }
 };
